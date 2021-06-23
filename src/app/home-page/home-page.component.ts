dac7719371edaf3b06a34f879ee739516432d91a
Jeff Buda <budajeff@gmail.com>
2021-06-22 23:43:32 -0400

    home page image appears

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ImageService } from '../image.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private imageService:ImageService) { }
+
+  public image: any;
 
   ngOnInit(): void {
+    const landingImage = this.imageService.getImages()['landing'];
+    this.image = {src: landingImage.image, alt: landingImage.alt};
   }
 
 }
