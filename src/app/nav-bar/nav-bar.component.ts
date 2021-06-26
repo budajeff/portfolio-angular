49124527d264bf263aee51393220c68b68d88afe
Jeff Buda <budajeff@gmail.com>
2021-06-26 07:28:28 -0400

    nav bar active item styling fixed font

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  private activeMenuText = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.activeMenuText = 'Home';
   }
 
+  public menuItemClicked(event:Event) {
+    this.activeMenuText = (event.currentTarget as HTMLElement).innerHTML;
+  }
+
+  public isActive(menuText:string) {
+    return menuText === this.activeMenuText;
+  }
 }
